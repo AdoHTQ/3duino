c4743c0b312409bf27d9ac894d475a218d44ccfe
Caden Feller <caden.feller@gmail.com>
2024-10-03 21:14:31 -0700

    readded custom plane mesh

--- 3duino/PlaneMesh.h
+++ 3duino/PlaneMesh.h
@@ -1,6 +1,6 @@
 // vertex array
-Vector vectors[] = {Vector(-1.0, -1.0, -0.0),Vector(1.0, -1.0, -0.0),Vector(-1.0, 1.0, 0.0),Vector(1.0, 1.0, 0.0),};
+Vector vectors[] = {Vector(0.0, 0.001998, -0.22573)/*Top*/,Vector(0.35355, 0.20479, -0.51534)/*Bottom RIght*/,Vector(0.0, -0.40758, -0.51252)/*Left*/,Vector(-0.35355, 0.20479, -0.51534)/*Top Right*/,Vector(0.0, 0.40758, -0.80496)/*Right*/,Vector(-0.35355, -0.20479, -0.80213)/*Top Left*/,Vector(0.35355,-0.20479,-0.80213)/*Bottom Left*/};
 // face array
-VectorI faces[] = {VectorI(2, 3, 1),VectorI(2, 4, 3),};
+VectorI faces[] = {VectorI(0, 1, 2),VectorI(0, 1, 3),VectorI(0, 2, 3),VectorI(1, 3, 4),VectorI(2, 3, 5),VectorI(2, 1, 6)};
 // final mesh object
-Mesh plane(vectors, faces, 4, 2);
+Mesh plane(vectors, faces, 7, 6);
