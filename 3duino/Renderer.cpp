9bf044b4d83bd67ef7ac513354ac3ea951f5b5d7
AdoHTQ <adamandperry@gmail.com>
2024-10-17 12:46:33 -0700

    🤣🔫

--- 3duino/Renderer.cpp
+++ 3duino/Renderer.cpp
@@ -52,16 +52,17 @@ void Renderer::createProjectionMatrix()
 VectorI* Renderer::transformVertex(Vector* ver)
 {
   Vector vertex = *ver;
-  if (vertex.axisCount < 3) {return VectorI();}
+  //if (vertex.axisCount < 3) {return new VectorI();}
   
   Vector homogenous = Vector(vertex[0], vertex[1], vertex[2], 1.0); 
   
-  // Perspective projection
+  //Perspective projection
   homogenous = projection * homogenous;
   
-  // Orthographic projection
-  Serial.println("fesnuoifsdj");
-  return new VectorI(round((homogenous[0] / homogenous[3] + 1.0) * dis->resX / 2.0), round((homogenous[1] / homogenous[3] + 1.0) * dis->resX / 2.0));
+  //Orthographic projection
+  //return new VectorI(round((homogenous[0] / homogenous[3] + 1.0) * dis->resX / 2.0), round((homogenous[1] / homogenous[3] + 1.0) * dis->resX / 2.0));
+  return new VectorI(homogenous[0], homogenous[1], homogenous[2]);
+  //return new VectorI(0, 0, 0);
 }
 
 void Renderer::renderMesh(Mesh *mesh)
@@ -85,17 +86,19 @@ void Renderer::renderMesh(Mesh *mesh)
     //Serial.println(sizeof(mesh->vertices));
     //Serial.println(mesh->vertices[mesh->faces[0][0]-1][0]);
     //Vector3 transform = Vector3(0.0, 0.0, 0.0);
-    VectorI p1 = transformVertex(&mesh->vertices[mesh->faces[i][0]-1]);
+    VectorI* p1 = transformVertex(&mesh->vertices[mesh->faces[i][0]-1]);
+    //VectorI tmp = *p1;
     //VectorI p2 = transformVertex(mesh->vertices[mesh->faces[i][1]-1]);
     //VectorI p3 = transformVertex(mesh->vertices[mesh->faces[i][2]-1]);
-    Serial.println(p1[0]);
-    Serial.println(p1[1]);
+    //Serial.println(tmp[0]);
+    //Serial.println(tmp[1]);
     // Serial.println(p2[0]);
     // Serial.println(p2[1]);
     // Serial.println(p3[0]);
     // Serial.println(p3[1]);
     //dis->drawTriangle(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1], 0xFFFF);
     //dis->drawTriangle(20, 20, 96, 64, 10, 40, 0xFFFF);
+    //delete p1;
   }
 }
 
