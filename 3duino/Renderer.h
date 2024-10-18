05e42f3122f450ad12ad6e5db68a2abe09a34309
AdoHTQ <adamandperry@gmail.com>
2024-10-18 08:58:40 -0700

    Update Renderer.h

--- 3duino/Renderer.h
+++ 3duino/Renderer.h
@@ -2,13 +2,11 @@
 #define RENDERER_H
 
 #include <Arduino.h>
-#include "Vectors.h"
-#include "DisplayDriver.h"
-#include "Matrix.h"
+#include <BasicLinearAlgebra.h>
 
 struct Mesh {
-  Vector* vertices;
-  VectorI* faces;
+  Matrix<3,1>* vertices;
+  Matrix<3,1,int>* faces;
 
   int numVertices;
   int numFaces;
@@ -17,7 +15,7 @@ struct Mesh {
 
   Mesh (int, int);
 
-  Mesh (Vector*, VectorI*, int, int);
+  Mesh (Matrix<3,1>*, Matrix<3,1,int>*, int, int);
 
   ~Mesh();
 
@@ -26,20 +24,20 @@ struct Mesh {
 class Renderer {
 private:
   DisplayDriver* dis;
-  Matrix44 projection;
+  Matrix<4,4> projection;
 
   float fov = 30 / 57.2957795;
   const float aspect = 1.;
   const float far = 10.;
   const float near = 0.1;
 
-  void drawTriangle(VectorI p1, VectorI p2, VectorI p3);
+  void drawTriangle(Matrix<2,1,int> p1, Matrix<2,1,int> p2, Matrix<2,1,int> p3);
 
   void createProjectionMatrix();
-  VectorI* transformVertex(Vector* vertex);
+  Matrix<3,1,int>* transformVertex(Vector* vertex);
 
 public:
-  Renderer(DisplayDriver* displayDriver);
+  Renderer(Matrix<2,1,int> resolution);
   ~Renderer();
 
   void renderMesh(Mesh* mesh);
